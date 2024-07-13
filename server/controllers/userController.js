070d71b424d06ba742b4616dcf9c52c08b14ee64
Abdul-Farshad <abdulfarshad13@gmail.com>
2024-07-13 18:13:02 +0530

    create test API route

---
+++ server/controllers/userController.js
@@ -0,0 +1,8 @@
+const test = (req, res) => {
+  res.json({
+    message: "hello world",
+  });
+};
+
+
+module.exports = test
--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connectDB = require("./dbConfig");
 
+const userRoute = require("./routes/userRoute.js")
+
 // Database connection
 connectDB(process.env.MONGO_DB_URI);
 
 const app = express();
-
 app.listen(3000, () => console.log("server running on PORT 3000"));
+
+app.use('/', userRoute)
